0bde05cf36165f3913b716a82f5bfdfc4041f2fc
Bikash Sharma <bikashsharma5151@gmail.com>
2026-03-17 13:46:44 +0000

    added

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("🚀 Java is running successfully in the Cloud Cluster!");
+        System.out.println("Environment: Docker Sidecar (Eclipse Temurin 17)");
+    }
+}
